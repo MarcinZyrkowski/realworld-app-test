d5081fb55a2ce9ac40e2cf529273e9452d47fccb
mzyrkowski <marcinzyrkowski7@gmail.com>
2025-06-03 17:25:38 +0200

    completed register new user test

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 ## Description
+
 Test automation project for realworld-app
 (should be cloned and run separately -
 [realworld-app](https://github.com/MarcinZyrkowski/realworld-app) )
 
 ## App url
+
 `http://localhost:3000`
 
 ## running tests
+
 `npx playwright test` - run all tests on all environments ("dev" and "qa")
 
 possible flags:
@@ -14,7 +17,7 @@ possible flags:
 | --- | --- |
 | `--ui` | enables UI mode |
 | `--headed` | enables headed mode, opened browser is visible |
-| `--project=dev` | specifies project 
+| `--project=dev` | specifies project
 
 flags could be mixed e.g.
 `npx playwright test --ui --project=dev`
--- tests/e2e/tests/RegisterNewUser.spec.ts
+++ tests/e2e/tests/RegisterNewUser.spec.ts
@@ -4,9 +4,11 @@ import { SignUpPage } from "../page/SignUpPage";
 
 test.describe("register new user", () => {
   let signInPage: SignInPage;
+  let signUpPage: SignUpPage;
 
   test.beforeEach(async ({ page }) => {
     signInPage = new SignInPage(page);
+    signUpPage = new SignUpPage(page);
   });
 
   test("register new user", async ({ page }) => {
@@ -17,5 +19,17 @@ test.describe("register new user", () => {
     await signInPage.signUpButton.click();
     await signInPage.signUpButton.click();
     await expect(page).toHaveURL(SignUpPage.url);
+
+    // TODO: generate random user data
+    await signUpPage.fillForm({
+      firstName: "John",
+      lastName: "Doe",
+      userName: "johndoe",
+      password: "password123",
+      confirmPassword: "password123",
+    });
+
+    await signUpPage.signUpButton.click();
+    await expect(page).toHaveURL(SignInPage.url);
   });
 });
