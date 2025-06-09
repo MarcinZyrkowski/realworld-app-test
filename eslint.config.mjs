2b65fdf40bec198f9207435af72d1551c9c0a4e7
mzyrkowski <marcinzyrkowski7@gmail.com>
2025-06-09 18:27:50 +0200

    linter

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    // ignores packages runtime generated and test reports
+    ignores: ["**/node_modules/**", "**/reports/**", "**/playwright-report/**"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+]);
